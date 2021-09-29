2fde6c1ee4aeca4570ec6f8015e220f37868739d
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-09-28 22:18:08 -0500

    feat: implement fake data for categories endpoints

--- src/resources/categories/categories.controller.ts
+++ src/resources/categories/categories.controller.ts
@@ -1,10 +1,20 @@
-import { generateFakeCategory } from '@utils/faker';
+import { generateFakeCategory, generateFakeCourses } from '@utils/faker';
 import { TController } from '../types';
 
 const categoriesController: TController = {
   async getAll(req, res) {
     const fakeCategories = Array.from({ length: 30 }).map(generateFakeCategory);
     res.send(fakeCategories);
+  },
+  async getById(req, res) {
+    res.send(generateFakeCategory());
+  },
+  async getCoursesById(req, res) {
+    const data = {
+      ...generateFakeCategory(),
+      courses: generateFakeCourses(),
+    };
+    res.send(data);
   },
 };
 
--- src/resources/categories/categories.router.ts
+++ src/resources/categories/categories.router.ts
@@ -5,5 +5,10 @@ import categoriesController from './categories.controller';
 const categoriesRouter = Router();
 
 categoriesRouter.get('/categories/', categoriesController.getAll);
+categoriesRouter.get('/categories/:id/', categoriesController.getById);
+categoriesRouter.get(
+  '/categories/:id/courses/',
+  categoriesController.getCoursesById
+);
 
 export default categoriesRouter;
