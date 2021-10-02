8be78ef18e9a8d6a5f80cd7b00902d8243a51deb
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-10-02 17:43:45 -0500

    feat: add jwt middleware to shopping cart endpoints
    User must be logged to return their shopping cart data

--- src/resources/shopping-cart/shopping-cart.router.ts
+++ src/resources/shopping-cart/shopping-cart.router.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import shoppingCartController from './shopping-cart.controller';
+import { verifyTokenAndAttachUserToRequest } from '../user/middlewares/authentication';
 
 const shoppingCartRouter = Router();
 
-shoppingCartRouter.get('/shopping-cart/', shoppingCartController.getAll);
+shoppingCartRouter.get(
+  '/shopping-cart/',
+  verifyTokenAndAttachUserToRequest,
+  shoppingCartController.getAll
+);
 
 export default shoppingCartRouter;
