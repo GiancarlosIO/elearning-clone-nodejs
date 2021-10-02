6f4e059d7225902aec6c57693da02e9f521d25f0
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-10-02 17:23:38 -0500

    feat: format subCategories and shoppingcart responses

--- src/resources/shopping-cart/shopping-cart.controller.ts
+++ src/resources/shopping-cart/shopping-cart.controller.ts
@@ -1,9 +1,11 @@
 import { generateShoppingCartFake } from '@utils/faker';
+import { StatusCodes } from 'http-status-codes';
 import { TController } from '../types';
+import { createResponse } from '@/utils/response';
 
 const shoppingCartController: TController = {
   async getAll(req, res) {
-    res.send(generateShoppingCartFake());
+    res.send(createResponse(generateShoppingCartFake(), StatusCodes.OK));
   },
 };
 
--- src/resources/subCategories/subCategories.controller.ts
+++ src/resources/subCategories/subCategories.controller.ts
@@ -1,23 +1,26 @@
-import { TController } from '../types';
+import { StatusCodes } from 'http-status-codes';
 
 import { generateFakeSubCategory, generateFakeCourses } from '@/utils/faker';
+import { createResponse } from '@/utils/response';
+
+import { TController } from '../types';
 
 const subCategoriesController: TController = {
   async getAll(req, res) {
     const subCategoriesFake = Array.from({ length: 10 }).map(
       generateFakeSubCategory
     );
-    res.send(subCategoriesFake);
+    res.send(createResponse(subCategoriesFake, StatusCodes.OK));
   },
   async getById(req, res) {
-    res.send(generateFakeSubCategory());
+    res.send(createResponse(generateFakeSubCategory(), StatusCodes.OK));
   },
   async getCoursesById(req, res) {
     const data = {
       ...generateFakeSubCategory(),
       courses: generateFakeCourses(),
     };
-    res.send(data);
+    res.send(createResponse(data, StatusCodes.OK));
   },
 };
 export default subCategoriesController;
